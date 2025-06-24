603d8677d19d9237f18cdd025819118c588b60b5
Jay Bagchi <joy.bagchi@gmail.com>
2025-06-23 20:13:45 -0700

    Completed Local LLM App

--- local_llm_vector_store.py
+++ local_llm_vector_store.py
@@ -1,6 +1,36 @@
-from sentence_transformers import SentenceTransformer
+import os
+import faiss
+from uuid import uuid4
+from langchain_community.vectorstores import FAISS
+from langchain_community.docstore import InMemoryDocstore
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+from langchain.chat_models import init_chat_model
 
+docs_dir = "docs"
 documents = []
-embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
-document_embeddings = embedding_model.encode(documents)
+for root, dirs, files in os.walk(docs_dir):
+    for file in filter(lambda ff: ff.endswith('.txt'), files):
+        with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
+            print(f"Processing file: {os.path.join(root, file)}")
+            content = f.read()
+            if content.strip():
+                doc = Document(page_content=content, metadata={"source": os.path.join(root, file)})
+                documents.append(doc)
+
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+index = faiss.IndexFlatL2(len(embeddings.embed_query("hello world")))  # Create a flat index for L2 distance
+vector_store = FAISS(
+    embedding_function=embeddings,
+    index=index,
+    docstore=InMemoryDocstore(),
+    index_to_docstore_id={},
+)
+
+uuids = [str(uuid4()) for _ in range(len(documents))]
+vector_store.add_documents(documents=documents, ids=uuids)
+print(vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 5}))
 
